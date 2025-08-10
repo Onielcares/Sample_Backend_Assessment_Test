1e87f55e9952f1210384e9ea4c2f876441738a20
Onielcares <dehinbonatt@gmail.com>
2025-08-10 07:46:49 +0100

    add corrections

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,9 +1,14 @@
-const { createUser, findByEmail, findById } = require("./user.repository");
+const {
+  createUser,
+  findByEmail,
+  findById,
+  updateUser,
+} = require("./user.repository");
 const messageRepository = require("./message.repository");
 const callRepository = require("./call.repository");
 
 module.exports = {
-  userRepository: { createUser, findByEmail, findById },
+  userRepository: { createUser, findByEmail, findById, updateUser },
   messageRepository,
   callRepository,
 };
